e7df96c8b41db6496588ff6762d56468578f12f6
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2024-01-01 19:23:06 +0100

    Init

---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sergey Zhiltsov on 01/01/2024.
+//
+
+#include "main.h"
---
+++ src/main.h
@@ -0,0 +1,14 @@
+//
+// Created by Sergey Zhiltsov on 01/01/2024.
+//
+
+#ifndef DUINO_ESP32_MAIN_H
+#define DUINO_ESP32_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //DUINO_ESP32_MAIN_H
